b406825e1150a610e2b7bba5be08ce1ffb232569
musabeytekin <02190201003@ogr.inonu.edu.tr>
2023-08-03 14:04:10 +0300

    simple rest controllers added

---
+++ src/main/java/com/musabeytekin/demo/rest/StudentRestController.java
@@ -0,0 +1,37 @@
+package com.musabeytekin.demo.rest;
+
+import com.musabeytekin.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> theStudents;
+
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
+        theStudents.add(new Student("Joey", "Mario"));
+        theStudents.add(new Student("Joey", "Mario"));
+        theStudents.add(new Student("Joey", "Mario"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return theStudents;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudents(@PathVariable int studentId) {
+        return theStudents.get(studentId);
+    }
+}
